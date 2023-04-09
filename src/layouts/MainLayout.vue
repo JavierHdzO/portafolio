8c7c7c094b4cf9bd15d2f06958608aa2e2f5cd66
JavierHdzO <fco_hernandez1705@hotmail.com>
2023-04-09 12:41:47 -0600

    040923 F [CHORE]: project base

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,93 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar
+        class="bg-cyan-8"
+      >
+        <q-btn
+          flat
+          dense
+          round
+          icon="las la-bars"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Francisco
+        </q-toolbar-title>
+
+        <div>v.1</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      :dark="true"
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
+
+const essentialLinks = [
+  {
+    title: 'About',
+    caption: 'A little about me',
+    icon: 'las la-info',
+    link: '#'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/JavierHdzO',
+    icon: 'lab la-github',
+    link: 'https://github.com/javierhdzo'
+  },
+  {
+    title: 'Linkedin',
+    caption: 'Francisco Javier Hdz O',
+    icon: 'lab la-linkedin-in',
+    link: 'https://www.linkedin.com/in/francisco-hernandez-ortiz/'
+  },
+  {
+    title: 'Facebook',
+    caption: '@SmJavier17',
+    icon: 'lab la-facebook',
+    link: 'https://www.facebook.com/SmJavier17'
+  },
+  {
+    title: 'Email',
+    caption: 'Send Personal Email',
+    icon: 'las la-envelope',
+    link: 'mailto:francisco.j.hdz.o@outlook.es?subject="Professional"&body="I am contact you to discuss professional business matter"'
+  }
+];
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,151 @@
+<template>
+  <q-page class="row items-center justify-evenly bg-grey-10">
+<div class="container">
+  <div class="top">
+    <div class="square">
+      <div class="square">
+        <div class="square">
+          <div class="square">
+            <div class="square"><div class="square">
+            </div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="bottom">
+    <div class="square">
+      <div class="square">
+        <div class="square">
+          <div class="square">
+            <div class="square"><div class="square">
+            </div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="left">
+    <div class="square">
+      <div class="square">
+        <div class="square">
+          <div class="square">
+            <div class="square"><div class="square">
+            </div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="right">
+    <div class="square">
+      <div class="square">
+        <div class="square">
+          <div class="square">
+            <div class="square"><div class="square">
+            </div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  </q-page>
+</template>
+
+<style>
+.container {
+  position: absolute;
+  top: 45%;
+  left: 48%;
+}
+
+.square {
+  width: 8px;
+  height: 30px;
+  background: #0097a7;
+  border-radius: 10px;
+  display: block;
+  /*margin:10px;*/
+  -webkit-animation: turn 2.5s ease infinite;
+  animation: turn 2.5s ease infinite;
+  -webkit-box-shadow: #0097a7 0px 1px 15px 0px;
+          box-shadow: #0097a7 0px 1px 15px 0px;
+}
+
+.top {
+  position: absolute;
+  left: 40%;
+  top: 50%;
+  -webkit-transform: rotate(90deg);
+  -ms-transform: rotate(90deg);
+      transform: rotate(90deg);
+}
+
+.bottom {
+  position: absolute;
+  left: 40%;
+  top: 50%;
+  -webkit-transform: rotate(-90deg);
+  -ms-transform: rotate(-90deg);
+      transform: rotate(-90deg);
+}
+
+.left {
+  position: absolute;
+  left: 40%;
+  top: 50%;
+}
+
+.right {
+  position: absolute;
+  left: 40%;
+  top: 50%;
+  -webkit-transform: rotate(-180deg);
+  -ms-transform: rotate(-180deg);
+      transform: rotate(-180deg);
+}
+
+@-webkit-keyframes turn {
+  0% {
+    -webkit-transform: translateX(0) translateY(0) rotate(0);
+            transform: translateX(0) translateY(0) rotate(0);
+  }
+
+  50% {
+    -webkit-transform: translateX(400%) translateY(100%) rotate(90deg);
+            transform: translateX(400%) translateY(100%) rotate(90deg);
+  }
+
+  100% {
+    -webkit-transform: translateX(0) translateY(0) rotate(0);
+            transform: translateX(0) translateY(0) rotate(0);
+  }
+}
+
+@keyframes turn {
+  0% {
+    -webkit-transform: translateX(0) translateY(0) rotate(0);
+            transform: translateX(0) translateY(0) rotate(0);
+  }
+
+  70% {
+    -webkit-transform: translateX(400%) translateY(100%) rotate(90deg);
+            transform: translateX(400%) translateY(100%) rotate(90deg);
+  }
+
+  100% {
+    -webkit-transform: translateX(0) translateY(0) rotate(0);
+            transform: translateX(0) translateY(0) rotate(0);
+  }
+}
+</style>
+
+<script setup lang="ts">
+import { Todo, Meta } from 'components/models';
+import { ref } from 'vue';
+
+const meta = ref<Meta>({
+  totalCount: 1200
+});
+</script>
